1b4c6f3cd778e6f0ed56363a95e3c33ba0b5a6e9
Vlados <vlad13051999@gmail.com>
2019-10-08 23:54:48 +0300

    Add profile page layout

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import '../styles/Profile.scss'
 
 const Profile = (props) => {
-  const logout = (e) => {
-    localStorage.removeItem('token')
-    props.history.push('/')
-  }
+  const name = 'Kekster'
+  const balance = '100'
 
   return (
-    <div>
-      <p>Profile</p>
-      <Button variant='danger' onClick={logout}>Log out</Button>
-      <br />
-      <Button variant='outline-primary' className='mr-3'>Show QR</Button>
-      <Button variant='primary'>Open QR Reader</Button>
-    </div>
+    <Container fluid>
+      <Row className='justify-content-center'>
+        <Col lg={4} md={8} className='text-center app-col'>
+          <div className='avatar--container'>
+            <div className='avatar--content'></div>
+          </div>
+          <div className='mt-4 mb-5'>
+            <h3>{name}</h3>
+            <h3>Balance: {balance}</h3>
+          </div>
+          <Button variant='outline-primary' className='mr-3'>Show QR</Button>
+          <Button variant='primary'>Open QR Reader</Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
