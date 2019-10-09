3b1054c23548d351e393ca3b413c7e2c3b9f974e
Alexander <sashok935@gmail.com>
2019-10-09 21:41:48 +0300

    Refactor and clean up qr decoder component

--- src/components/QrReader.js
+++ src/components/QrReader.js
@@ -2,51 +2,45 @@ import React, { useEffect, useRef } from 'react'
 import jsQR from 'jsqr'
 
 const QrReader = (props) => {
-  let constraintDimensions = null
-  const { height, width } = window.screen
-  if (window.screen.orientation.type.includes('landscape')) {
-    constraintDimensions = { width, height }
-  } else {
-    // noinspection JSSuspiciousNameCombination
-    constraintDimensions = {
-      width: height,
-      height: width
-    }
-  }
+  const canvasRef = useRef()
+  const contextRef = useRef()
+  const video = document.createElement('video')
+  const { height, width, orientation } = window.screen
+  // noinspection JSSuspiciousNameCombination
+  const constraintDimensions = orientation.type.includes('landscape')
+    ? { width, height }
+    : { width: height, height: width }
+
   const constraints = {
     video: {
       facingMode: 'environment',
       ...constraintDimensions
     }
   }
-  const canvasRef = useRef()
-  const contextRef = useRef()
-  const video = document.createElement('video')
 
-  const tick = () => {
+  const drawFrame = () => {
+    const ctx = contextRef.current
+    const canvas = canvasRef.current
+    if (!canvas || !ctx) {
+      window.requestAnimationFrame(drawFrame)
+      return
+    }
+
     if (video.readyState === video.HAVE_ENOUGH_DATA) {
-      if (!contextRef.current || !canvasRef.current) {
-        window.requestAnimationFrame(tick)
-        return
-      }
-
-      const canvas = canvasRef.current
-      const ctx = contextRef.current
-
       canvas.height = video.videoHeight
       canvas.width = video.videoWidth
       ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
       const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+      const qrData = jsQR(imageData.data, imageData.width, imageData.height, {
         inversionAttempts: 'dontInvert'
       })
 
-      if (code && code.data) {
-        navigator.vibrate(200)
-        props.history.push('/transfer', { data: code.data })
+      if (qrData && qrData.data) {
+        navigator.vibrate(200) // doesn't work if user haven't tapped/focused on screen
+        props.history.push('/transfer', { data: qrData.data })
       }
     }
-    window.requestAnimationFrame(tick)
+    window.requestAnimationFrame(drawFrame)
   }
   useEffect(() => {
     contextRef.current = canvasRef.current.getContext && canvasRef.current.getContext('2d')
@@ -55,7 +49,7 @@ const QrReader = (props) => {
       video.srcObject = stream
       video.setAttribute('playsinline', true) // required to tell iOS safari we don't want fullscreen
       video.play()
-      window.requestAnimationFrame(tick)
+      window.requestAnimationFrame(drawFrame)
     })
 
     return () => {
